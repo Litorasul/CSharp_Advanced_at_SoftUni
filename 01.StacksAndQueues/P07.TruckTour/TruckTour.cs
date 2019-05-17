79b1d3844701115e624cf1f60c909d15403944bd
Nikolay Kelov <n_kelov@yahoo.com>
2019-05-17 16:08:55 +0200

    Problem 7 - 100%

---
+++ 01.StacksAndQueues/P07.TruckTour/TruckTour.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P07.TruckTour
+{
+    class TruckTour
+    {
+        static void Main(string[] args)
+        {
+            Queue<int[]> petrolPumps = new Queue<int[]>();
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                int[] petrolPump = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+
+                petrolPumps.Enqueue(petrolPump);
+            }
+
+            int index = 0;
+
+            while (true)
+            {
+                int totalFuel = 0;
+
+                foreach (var pump in petrolPumps)
+                {
+                    
+                    int petrol = pump[0];
+                    int distance = pump[1];
+
+                    totalFuel += petrol - distance;
+
+                    if (totalFuel < 0)
+                    {
+                        petrolPumps.Enqueue(petrolPumps.Dequeue());
+                        index++;
+                        break;
+                    }
+                }
+                if (totalFuel >= 0)
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine(index);
+        }
+    }
+}
