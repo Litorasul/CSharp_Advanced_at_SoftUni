7bcaafe0d1f4c097e3bf7984708a7e4644051c4b
Nikolay Kelov <n_kelov@yahoo.com>
2019-05-17 11:45:17 +0200

    Problem 4 100%

---
+++ 01.StacksAndQueues/P04.FastFood/FastFood.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P04.FastFood
+{
+    class FastFood
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> orders = new Queue<int>();
+            int allFood = int.Parse(Console.ReadLine());
+            int[] ordersInput = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            foreach (var order in ordersInput)
+            {
+                orders.Enqueue(order);
+            }
+
+            Console.WriteLine(orders.Max());
+
+            int currentOrder = orders.Peek();
+            while (allFood - currentOrder >= 0)
+            {
+                allFood -= currentOrder;
+                orders.Dequeue();
+                if (orders.Count != 0)
+                {
+                    currentOrder = orders.Peek();
+                }
+                else
+                {
+                    break;
+                }
+                
+            }
+
+            if (orders.Count == 0)
+            {
+                Console.WriteLine("Orders complete");
+            }
+            else
+            {
+                Console.WriteLine($"Orders left: {string.Join(" ", orders)}");
+            }
+        }
+    }
+}
