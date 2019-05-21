0d03dee23b9a935738a9db174b967ab1356b82e1
Nikolay Kelov <n_kelov@yahoo.com>
2019-05-21 18:59:03 +0200

    Problem 1 - 100%

---
+++ 02.MultidimensionalArrays/02.MultidimensionalArrays/P01.DiagonalDifference/DiagonalDifference.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace P01.DiagonalDifference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[size, size];
+
+            for (int row = 0; row < size; row++)
+            {
+                int[] elements = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+                for (int col = 0; col < size; col++)
+                {
+                    matrix[row, col] = elements[col];
+                }
+            }
+            int diagonalOne = 0;
+            int diagonalTwo = 0;
+            int cols = 0;
+            int cols2 = size - 1;
+            for (int rows = 0; rows < size; rows++)
+            {
+                diagonalOne += matrix[rows, cols];
+                cols++;
+                diagonalTwo += matrix[rows, cols2];
+                cols2--;
+            }
+            int sum = 0;
+            if (diagonalOne > diagonalTwo)
+            {
+                sum = diagonalOne - diagonalTwo;
+            }
+            else
+            {
+                sum = diagonalTwo - diagonalOne;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
