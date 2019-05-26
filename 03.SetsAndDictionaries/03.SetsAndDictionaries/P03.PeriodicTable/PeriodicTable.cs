965151a184112f9ebbd118563d066aab3fda2ce7
Nikolay Kelov <n_kelov@yahoo.com>
2019-05-26 09:40:28 +0200

    Problem 3 - 100%

---
+++ 03.SetsAndDictionaries/03.SetsAndDictionaries/P03.PeriodicTable/PeriodicTable.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P03.PeriodicTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = int.Parse(Console.ReadLine());
+
+            SortedSet<string> elements = new SortedSet<string>();
+
+            for (int i = 0; i < counter; i++)
+            {
+                string[] input = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                foreach (var item in input)
+                {
+                    elements.Add(item);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", elements));
+        }
+    }
+}
