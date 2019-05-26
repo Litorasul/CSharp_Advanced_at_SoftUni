20a75e82343f7ab12c531a63f91d1e293eab60b0
Nikolay Kelov <n_kelov@yahoo.com>
2019-05-26 10:08:14 +0200

    Problem 5 - 100%

---
+++ 03.SetsAndDictionaries/03.SetsAndDictionaries/P05.CountSymbols/CountSymbols.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P05.CountSymbols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            SortedSet<char> uniqueElements = new SortedSet<char>();
+
+            foreach (var item in input)
+            {
+                uniqueElements.Add(item);
+            }
+
+            foreach (var uniqueItem in uniqueElements)
+            {
+                int count = 0;
+
+                foreach (var item in input)
+                {
+                    if (uniqueItem == item)
+                    {
+                        count++;
+                    }
+                }
+                Console.WriteLine($"{uniqueItem}: {count} time/s");
+            }
+        }
+    }
+}
