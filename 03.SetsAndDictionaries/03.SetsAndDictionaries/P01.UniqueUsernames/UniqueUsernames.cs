50f71e801b0870a05f1fa0e8a1da78faacd30c98
Nikolay Kelov <n_kelov@yahoo.com>
2019-05-26 09:01:27 +0200

    Problem 1 - 100%

---
+++ 03.SetsAndDictionaries/03.SetsAndDictionaries/P01.UniqueUsernames/UniqueUsernames.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P01.UniqueUsernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = int.Parse(Console.ReadLine());
+
+            HashSet<string> set = new HashSet<string>();
+
+            for (int i = 0; i < counter; i++)
+            {
+                string input = Console.ReadLine();
+                set.Add(input);
+            }
+
+            foreach (var item in set)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
