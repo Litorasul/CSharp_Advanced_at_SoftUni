abc33d93685a00bdc033d67bee488755aed0446b
Nikolay Kelov <n_kelov@yahoo.com>
2019-06-01 20:30:34 +0200

    Problem 1

--- 04.StreamsFilesAndDirectories/04.StreamsFilesAndDirectory/P01.EvenLines/Program.cs
+++ 04.StreamsFilesAndDirectories/04.StreamsFilesAndDirectory/P01.EvenLines/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
 
 namespace P01.EvenLines
 {
@@ -6,7 +8,30 @@ namespace P01.EvenLines
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string path = "text.txt";
+
+            using (var reader = new StreamReader(path))
+            {
+                int count = 0;
+                string line = string.Empty;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if (count % 2 == 0)
+                    {
+                        line = line.Replace('-', '@');
+                        line = line.Replace(',', '@');
+                        line = line.Replace('.', '@');
+                        line = line.Replace('!', '@');
+                        line = (string)line.Replace('?', '@');
+                        string[] lineArr = line.Split();
+                        lineArr = lineArr.Reverse().ToArray();
+                        
+                        Console.WriteLine(string.Join(" ",lineArr));
+                        
+                    }
+                    count++;
+                }
+            }
         }
     }
 }
