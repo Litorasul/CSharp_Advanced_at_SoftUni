1a4c8fabfe357fd5e8306c16bdc8af26f0fa20d2
Nikolay Kelov <n_kelov@yahoo.com>
2019-06-03 12:44:11 +0200

    Problem 4

---
+++ 04.StreamsFilesAndDirectories/04.StreamsFilesAndDirectory/P04.BinaryCopyFile/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace P04.BinaryCopyFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string picPath = "copyMe.png";
+            string copyPath = "copyMe-copy.png";
+
+            using (FileStream reader = new FileStream(picPath, FileMode.Open))
+            {
+                using (FileStream writer = new FileStream(copyPath, FileMode.Create))
+                {
+                    while (true)
+                    {
+                        byte[] byteArr = new byte[4096];
+
+                        int size = reader.Read(byteArr, 0, byteArr.Length);
+
+                        if (size == 0)
+                        {
+                            break;
+                        }
+
+                        writer.Write(byteArr, 0, size);
+                    }
+                }
+            }
+        }
+    }
+}
