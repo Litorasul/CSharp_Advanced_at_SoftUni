b7541f17f4f668945843ceac19f55e9b90b98254
Nikolay Kelov <n_kelov@yahoo.com>
2019-06-03 11:01:52 +0200

    Problem 2

---
+++ 04.StreamsFilesAndDirectories/04.StreamsFilesAndDirectory/P02.LineNumbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace P02.LineNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("text.txt");
+
+            int counter = 1;
+
+            foreach (var line in lines)
+            {
+                int letterCount = line.Count(char.IsLetter);
+                int punctsCount = line.Count(char.IsPunctuation);
+
+                File.AppendAllText("output.txt", $"Line {counter++}: {line} ({letterCount})({punctsCount}){Environment.NewLine}");
+            }
+
+            
+        }
+    }
+}
