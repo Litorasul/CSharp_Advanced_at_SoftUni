4376e1f695663df36fe66e2d6a597ed8b4f4a686
Nikolay Kelov <n_kelov@yahoo.com>
2019-06-03 13:46:50 +0200

    Problem 6

---
+++ 04.StreamsFilesAndDirectories/04.StreamsFilesAndDirectory/P06.ZipAndExtract/ZipAndExtract.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+
+namespace P06.ZipAndExtract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = "copyMe.png";
+            string zipFile = "myZip.zip";
+
+            using (var archive = ZipFile.Open(zipFile, ZipArchiveMode.Create))
+            {
+                archive.CreateEntryFromFile(file, Path.GetFileName(file));
+            }
+        }
+    }
+}
