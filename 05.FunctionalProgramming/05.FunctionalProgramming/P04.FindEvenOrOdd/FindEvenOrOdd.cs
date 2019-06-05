5cc43c4c4b656ffb3df7e91b2e13e7ff952d7a23
Nikolay Kelov <n_kelov@yahoo.com>
2019-06-05 19:53:13 +0200

    Problem 4 - 100%

---
+++ 05.FunctionalProgramming/05.FunctionalProgramming/P04.FindEvenOrOdd/FindEvenOrOdd.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P04.FindEvenOrOdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int start = input[0];
+            int finish = input[1];
+
+            string filter = Console.ReadLine()
+                .ToLower();
+
+
+            List<int> numbers = new List<int>();
+
+            for (int i = start; i <= finish; i++)
+            {
+                numbers.Add(i);
+            }
+
+            if (filter == "even")
+            {
+                Console.WriteLine(string.Join(" ", numbers.Where(x => x % 2 == 0)
+                    .ToList()));
+                    
+            }
+            else
+            {
+                Console.WriteLine(string.Join(" ", numbers.Where(x => x % 2 != 0)
+                    .ToList()));
+            }
+
+
+        }
+
+    }
+}
