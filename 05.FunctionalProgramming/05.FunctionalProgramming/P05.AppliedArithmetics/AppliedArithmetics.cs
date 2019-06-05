2c662cdd412b04a42f8ddda118f66ae9a002bf27
Nikolay Kelov <n_kelov@yahoo.com>
2019-06-05 20:17:59 +0200

    Problem 5 - 100%

---
+++ 05.FunctionalProgramming/05.FunctionalProgramming/P05.AppliedArithmetics/AppliedArithmetics.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P05.AppliedArithmetics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+            string command = string.Empty;
+
+            while ((command = Console.ReadLine()) != "end")
+            {
+                if (command == "add")
+                {
+                    numbers = numbers.Select(n => n + 1).ToList();
+                }
+                else if (command == "multiply")
+                {
+                    numbers = numbers.Select(n => n * 2).ToList();
+                }
+                else if (command == "subtract")
+                {
+                    numbers = numbers.Select(n => n - 1).ToList();
+                }
+                else if (command == "print")
+                {
+                    Console.WriteLine(string.Join(" ", numbers));
+                }
+            }
+        }
+    }
+}
