4586c10a030e69fa5bc2c9ff598e41bf3ef2a9a6
Nikolay Kelov <n_kelov@yahoo.com>
2019-06-05 19:17:37 +0200

    Problem 2 - 100%

---
+++ 05.FunctionalProgramming/05.FunctionalProgramming/P02.KnightsOfHonor/KnightsOfHonor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace P02.KnightsOfHonor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string> print = m => Console.WriteLine($"Sir {m}");
+
+            Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList()
+                .ForEach(print);
+        }
+    }
+}
