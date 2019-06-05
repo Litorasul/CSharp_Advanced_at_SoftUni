654bafb4d90ef28af653eb861f8ff1d03da15d6e
Nikolay Kelov <n_kelov@yahoo.com>
2019-06-05 19:26:59 +0200

    Problem 3 - 100%

---
+++ 05.FunctionalProgramming/05.FunctionalProgramming/P03.CustomMinFunction/CustomMinFunction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace P03.CustomMinFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int[], int> smalest = n => n.Min();
+
+            int[] input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Console.WriteLine(smalest(input));
+        }
+    }
+}
