812137e62b98ee1427621ab3b4d220c94ba91c95
Nikolay Kelov <n_kelov@yahoo.com>
2019-06-05 19:12:19 +0200

    Problem 1 - 100%

---
+++ 05.FunctionalProgramming/05.FunctionalProgramming/P01.ActionPoint/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P01.ActionPoint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string> print = m => Console.WriteLine(m);
+
+            Console.ReadLine()
+                .Split()
+                .ToList()
+                .ForEach(print);
+           
+        }
+    }
+}
