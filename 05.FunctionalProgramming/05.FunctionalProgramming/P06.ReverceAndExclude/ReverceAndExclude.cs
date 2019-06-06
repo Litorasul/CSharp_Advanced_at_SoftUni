d8a7205dd8d42bcc415e97b622192da041715034
Nikolay Kelov <n_kelov@yahoo.com>
2019-06-06 18:41:02 +0200

    Problem 6 - 100%

---
+++ 05.FunctionalProgramming/05.FunctionalProgramming/P06.ReverceAndExclude/ReverceAndExclude.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace P06.ReverceAndExclude
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] collection = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .Reverse()
+                .ToArray();
+
+            int devisor = int.Parse(Console.ReadLine());
+
+            collection = collection.Where(n => n % devisor != 0)
+                .ToArray();
+
+            Console.WriteLine(string.Join(" ", collection));
+        }
+    }
+}
