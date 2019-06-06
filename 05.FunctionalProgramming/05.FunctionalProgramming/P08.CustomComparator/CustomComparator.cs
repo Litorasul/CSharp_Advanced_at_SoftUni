9a924a797101bc4a7f115afda0cfeb8052f17502
Nikolay Kelov <n_kelov@yahoo.com>
2019-06-06 19:10:23 +0200

    Problem 8 - 100%

---
+++ 05.FunctionalProgramming/05.FunctionalProgramming/P08.CustomComparator/CustomComparator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace P08.CustomComparator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] even = input.Where(n => n % 2 == 0)
+                .OrderBy(n => n)
+                .ToArray();
+
+            int[] odd = input.Where(n => n % 2 != 0)
+                .OrderBy(n => n)
+                .ToArray();
+
+            int[] result = new int[input.Length];
+            int counter = 0;
+
+            foreach (var number in even)
+            {
+                result[counter] = number;
+                counter++;
+            }
+
+            foreach (var number in odd)
+            {
+                result[counter] = number;
+                counter++;
+            }
+
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
