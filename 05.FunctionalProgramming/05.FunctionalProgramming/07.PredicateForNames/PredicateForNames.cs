65b5e3c9be5c5c6a7723c80f11386d922852fe95
Nikolay Kelov <n_kelov@yahoo.com>
2019-06-06 18:49:35 +0200

    Problem 7 - 100%

---
+++ 05.FunctionalProgramming/05.FunctionalProgramming/07.PredicateForNames/PredicateForNames.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace _07.PredicateForNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxLenght = int.Parse(Console.ReadLine());
+
+            Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Where(n => n.Length <= maxLenght)
+                .ToList()
+                .ForEach(Console.WriteLine);
+        }
+    }
+}
