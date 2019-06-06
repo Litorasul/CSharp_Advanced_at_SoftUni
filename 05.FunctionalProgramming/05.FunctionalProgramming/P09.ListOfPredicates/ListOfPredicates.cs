b441e5875f63eb68ca33dc39ca7a8f0e7df03435
Nikolay Kelov <n_kelov@yahoo.com>
2019-06-06 19:22:14 +0200

    Problem 9 - 100%

---
+++ 05.FunctionalProgramming/05.FunctionalProgramming/P09.ListOfPredicates/ListOfPredicates.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P09.ListOfPredicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int capacity = int.Parse(Console.ReadLine());
+
+            List<int> numbers = new List<int>();
+
+            for (int i = 1; i <= capacity; i++)
+            {
+                numbers.Add(i);
+            }
+
+            int[] devisors = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            foreach (var number in devisors)
+            {
+                numbers = numbers.Where(n => n % number == 0)
+                    .ToList();
+            }
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
