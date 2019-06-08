b9c5791b7706790d7370f6d6ca74b642e58431c1
Nikolay Kelov <n_kelov@yahoo.com>
2019-06-08 11:33:20 +0200

    Problem 12 - 100%

---
+++ 05.FunctionalProgramming/05.FunctionalProgramming/P12.TriFunction/TriFunction.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace P12.TriFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lenght = int.Parse(Console.ReadLine());
+
+            string[] names = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            Func<string, int, bool> isLarger = (name, charsLenght)
+                => name.Sum(x => x) >= charsLenght;
+
+            Func<string[], Func<string, int, bool>, string> namesFilter =
+                (inputNames, isLargerFunc)
+                => inputNames.FirstOrDefault(x => isLargerFunc(x, lenght));
+
+            string result = namesFilter(names, isLarger);
+
+            Console.WriteLine(result);
+
+
+        }
+    }
+}
