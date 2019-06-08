fe7e40da6cf17a8db4e02bdce8079ff8b2e9e1b0
Nikolay Kelov <n_kelov@yahoo.com>
2019-06-08 11:11:54 +0200

    Problem 10 - not finished, Problem 11 - 100%

---
+++ 05.FunctionalProgramming/05.FunctionalProgramming/P10.PredicateParty/PredicateParty.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+
+namespace P10.PredicateParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var partyGuests = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            string command = string.Empty;
+
+            while ((command = Console.ReadLine()) != "Party!")
+            {
+                string[] commArr = command
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string toDo = commArr[0].ToLower();
+                string action = commArr[1].ToLower();
+
+                if (toDo == "remove")
+                {
+                    if (action == "lenght")
+                    {
+                        int lenght = int.Parse(commArr[2]);
+
+                        partyGuests = partyGuests
+                            .Where(p => p.Count() <= lenght)
+                            .ToList();
+
+                    }
+                    else if (action == "startswith")
+                    {
+                        string beginning = commArr[2];
+                    }
+                    else if (action == "endswith")
+                    {
+                        string end = commArr[2];
+
+                    }
+                }
+                else
+                {
+                    if (action == "lenght")
+                    {
+                        int lenght = int.Parse(commArr[2]);
+
+                    }
+                    else if (action == "startswith")
+                    {
+                        string beginning = commArr[2];
+                    }
+                    else if (action == "endswith")
+                    {
+                        string end = commArr[2];
+
+                    }
+
+                }
+
+
+            }
+        }
+    }
+}
---
+++ 05.FunctionalProgramming/05.FunctionalProgramming/P11.PartyReservationFilterModule/PartyReservarionFilterMode.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P11.PartyReservationFilterModule
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine()
+                .Split()
+                .ToArray();
+
+            string filter = string.Empty;
+
+            List<string> filters = new List<string>();
+
+            while ((filter = Console.ReadLine()) != "Print")
+            {
+                string[] filterInfo = filter
+                    .Split(";", StringSplitOptions.RemoveEmptyEntries);
+
+                string action = filterInfo[0];
+
+                if (action == "Add filter")
+                {
+                    filters.Add($"{filterInfo[1]};{filterInfo[2]}");
+                }
+                else if (action == "Remove filter")
+                {
+                    filters.Remove($"{filterInfo[1]};{filterInfo[2]}");
+                }
+            }
+
+            Func<string, int, bool> lenghtFilter = (name, lenght) => name.Length == lenght;
+            Func<string, string, bool> startsWithFilter = (name, param) => name.StartsWith(param);
+            Func<string, string, bool> endsWithFilter = (name, param) => name.EndsWith(param);
+            Func<string, string, bool> containsFilter = (name, param) => name.Contains(param);
+
+            foreach (var act in filters)
+            {
+                string[] actInfo = act.Split(";");
+
+                string action = actInfo[0];
+                string param = actInfo[1];
+
+                if (action == "Starts with")
+                {
+                    names = names.Where(name => !startsWithFilter(name, param))
+                        .ToArray();
+                }
+                else if (action == "Ends with")
+                {
+                    names = names.Where(name => !endsWithFilter(name, param))
+                        .ToArray();
+                }
+                else if (action == "Contains")
+                {
+                    names = names.Where(name => !containsFilter(name, param))
+                        .ToArray();
+                }
+                else if (action == "Lenght")
+                {
+                    int lenght = int.Parse(param);
+                    names = names.Where(name => !lenghtFilter(name, lenght))
+                        .ToArray();
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", names));
+        }
+    }
+}
