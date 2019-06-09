94b6337d5e22f05a9b164c2bcb2de5d08acff288
Nikolay Kelov <n_kelov@yahoo.com>
2019-06-09 17:36:42 +0200

    Problem 2 - 100%

--- 06.DefiningClasses/06.DefiningClasses/06.DefiningClasses/Person.cs
+++ 06.DefiningClasses/06.DefiningClasses/06.DefiningClasses/Person.cs
@@ -19,5 +19,22 @@ namespace DefiningClasses
             get => this.age;
             set=> this.age = value;
         }
+
+        public Person()
+        {
+            name = "No name";
+            age = 1;
+        }
+
+        public Person(int age): this()
+        {
+            this.age = age; 
+        }
+
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
     }
 }
