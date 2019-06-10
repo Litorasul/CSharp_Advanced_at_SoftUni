6dfd9789924cca42127282b7733d1e7dba2f61fa
Nikolay Kelov <n_kelov@yahoo.com>
2019-06-10 21:47:40 +0200

    Problem 05 - 100%

---
+++ 06.DefiningClasses/06.DefiningClasses/P05.DateModifier/DateModifier.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace P05.DateModifier
+{
+    public class DateModifier
+    {
+        private int dateDifference;
+
+        public DateModifier(string dateOne, string dateTwo)
+        {
+            int[] first = dateOne
+             .Split().Select(int.Parse).ToArray();
+            DateTime firstDate = new DateTime(first[0], first[1], first[2]);
+            int[] second = dateTwo
+             .Split().Select(int.Parse).ToArray();
+            DateTime secondDate = new DateTime(second[0], second[1], second[2]);
+
+            if (firstDate > secondDate)
+            {
+                dateDifference = (firstDate - secondDate).Days;
+            }
+            else
+            {
+                dateDifference = (secondDate - firstDate).Days;
+            }
+
+        }
+
+        public int TheDifferance()
+        {
+            return dateDifference;
+        }
+
+    }
+}
---
+++ 06.DefiningClasses/06.DefiningClasses/P05.DateModifier/Problem05.cs
@@ -0,0 +1,20 @@
+﻿using P05.DateModifier;
+using System;
+
+namespace DefiningClasses
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string dateOne = Console.ReadLine();
+            string dateTwo = Console.ReadLine();
+
+            DateModifier dateModifier = new DateModifier(dateOne, dateTwo);
+
+            int result = dateModifier.TheDifferance();
+
+            Console.WriteLine(result);
+        }
+    }
+}
