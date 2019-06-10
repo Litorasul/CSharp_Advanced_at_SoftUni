5165887d46e20485c70f487e7090246ff53ef20a
Nikolay Kelov <n_kelov@yahoo.com>
2019-06-10 20:08:33 +0200

    Problem 4  - 100%

---
+++ 06.DefiningClasses/06.DefiningClasses/P04.OpinionPoll/OpinionPoll.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int counter = int.Parse(Console.ReadLine());
+            List<Person> peoples = new List<Person>();
+
+            for (int i = 0; i < counter; i++)
+            {
+                string[] personsInput = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string personName = personsInput[0];
+                int personAge = int.Parse(personsInput[1]);
+
+                Person person = new Person(personName, personAge);
+
+                peoples.Add(person);
+            }
+
+            var result = peoples.Where(p => p.Age > 30)
+                .ToList();
+
+            foreach (var pers in result.OrderBy(p => p.Name))
+            {
+                Console.WriteLine($"{pers.Name} - {pers.Age}");
+            }
+        }
+    }
+}
