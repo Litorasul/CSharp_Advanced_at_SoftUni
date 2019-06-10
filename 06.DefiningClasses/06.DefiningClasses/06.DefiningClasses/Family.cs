54641ab646b76d6d1befee411f94d4a514da2956
Nikolay Kelov <n_kelov@yahoo.com>
2019-06-10 19:38:10 +0200

    Problem 3 - 100%

---
+++ 06.DefiningClasses/06.DefiningClasses/06.DefiningClasses/Family.cs
@@ -0,0 +1,31 @@
+﻿using DefiningClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _06.DefiningClasses
+{
+    public class Family
+    {
+        private List<Person> familyMembers;
+
+        public Family()
+        {
+            familyMembers = new List<Person>();
+        }
+
+        public void AddMember(Person member)
+        {
+            familyMembers.Add(member);
+        }
+
+        public Person GetOldestMember()
+        {
+            Person oldestMember = familyMembers.OrderByDescending(p => p.Age)
+                .FirstOrDefault();
+
+            return oldestMember;
+        }
+    }
+}
--- 06.DefiningClasses/06.DefiningClasses/06.DefiningClasses/Program.cs
+++ 06.DefiningClasses/06.DefiningClasses/06.DefiningClasses/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _06.DefiningClasses;
+using System;
 
 namespace DefiningClasses
 {
@@ -6,7 +7,22 @@ namespace DefiningClasses
     {
         static void Main(string[] args)
         {
-           
+            int counter = int.Parse(Console.ReadLine());
+
+            Family family = new Family();
+
+            for (int i = 0; i < counter; i++)
+            {               
+                string[] input = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                Person person = new Person(input[0], int.Parse(input[1]));
+                family.AddMember(person);
+            }
+
+            Person oldest = family.GetOldestMember();
+
+            Console.WriteLine($"{oldest.Name} {oldest.Age}");
         }
     }
 }
