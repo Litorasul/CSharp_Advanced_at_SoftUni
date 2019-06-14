79182e0acccab3b9d5376009cb9504e114ec45d1
Nikolay Kelov <n_kelov@yahoo.com>
2019-06-14 20:07:10 +0200

    Problem 9 - 100%

---
+++ 06.DefiningClasses/06.DefiningClasses/P09.PokemonTrainer/Problem09.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P09.PokemonTrainer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = string.Empty;
+            Dictionary<string, Trainer> trainers = new Dictionary<string, Trainer>();
+
+            while ((input = Console.ReadLine()) != "Tournament")
+            {
+                string[] pokeInfo = input
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                //"{trainerName} {pokemonName} {pokemonElement} {pokemonHealth}"
+
+                string trainerName = pokeInfo[0];
+                string pokemonName = pokeInfo[1];
+                string pokemonElement = pokeInfo[2];
+                int pokemonHealth = int.Parse(pokeInfo[3]);
+                if (trainers.ContainsKey(trainerName))
+                {
+                    Pokemon pokemon = new Pokemon(pokemonName, pokemonElement, pokemonHealth);
+                    trainers[trainerName].Collection.Add(pokemon);
+
+                }
+                else
+                {
+                    Pokemon pokemon = new Pokemon(pokemonName, pokemonElement, pokemonHealth);
+                    Trainer trainer = new Trainer(trainerName, pokemon);
+                    trainers.Add(trainerName, trainer);
+                }
+
+
+
+            }
+
+            while ((input = Console.ReadLine()) != "End")
+            {
+                foreach (var trainer in trainers)
+                {
+                    bool elementExsist = false;
+
+                    foreach (var pokemon in trainer.Value.Collection)
+                    {
+                        if (pokemon.Element == input)
+                        {
+                            elementExsist = true;
+                        }
+                    }
+
+                    if (elementExsist)
+                    {
+                        trainer.Value.BadgeCounter++;
+                    }
+                    else
+                    {
+                        trainer.Value.RemoveTenHealth();
+                    }
+
+                    trainer.Value.CheckPokemonHealth();
+                }
+
+            }
+
+            foreach (var tr in trainers.OrderByDescending(x => x.Value.BadgeCounter))
+            {
+                Console.WriteLine(tr.Value.ToString());
+            }
+        }
+    }
+}
---
+++ 06.DefiningClasses/06.DefiningClasses/P09.PokemonTrainer/Trainer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace P09.PokemonTrainer
+{
+    class Trainer
+    {
+        public string Name { get; set; }
+        public int BadgeCounter { get; set; }
+
+        public List<Pokemon> Collection { get; set; }
+
+        public Trainer(string name, Pokemon pokemon)
+        {
+            Name = name;
+            BadgeCounter = 0;
+            Collection = new List<Pokemon>();
+            Collection.Add(pokemon);
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} {BadgeCounter} {Collection.Count}";
+        }
+
+        public void CheckPokemonHealth()
+        {
+            if (Collection.Count > 0)
+            {
+                Collection = Collection.Where(x => x.Health > 0).ToList();
+            }
+        }
+
+        public void RemoveTenHealth()
+        {
+            foreach (var pokemon in Collection)
+            {
+                pokemon.Health -= 10;
+            }
+        }
+
+    }
+}
