491c6e2f373347ae28cb5cb7825bf6477e37a191
Nikolay Kelov <n_kelov@yahoo.com>
2019-06-16 19:15:18 +0200

    Problem 03 - 100%

---
+++ 07.Generics/07.Generics/P03.GenericSwapString/GenericSwapString.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Generics
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+
+            List<string> theList = new List<string>();
+            
+
+            for (int i = 0; i < count; i++)
+            {
+                string input = Console.ReadLine();
+                theList.Add(input);
+            }
+
+            GenericSwap<string>(theList);
+        }
+
+        public static void GenericSwap<T>(List<T> list)
+        {
+            int[] swap = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int indexOne = swap[0];
+            int indexTwo = swap[1];
+
+            T temp = list[indexOne];
+            list[indexOne] = list[indexTwo];
+            list[indexTwo] = temp;
+
+            foreach (var item in list)
+            {
+                Console.WriteLine($"{item.GetType()}: {item}");
+            }
+
+        }
+    }
+}
