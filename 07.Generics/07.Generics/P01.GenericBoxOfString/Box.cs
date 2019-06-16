0b93f03f103a68e63b5de60a994be7e1674665e4
Nikolay Kelov <n_kelov@yahoo.com>
2019-06-16 18:37:14 +0200

    Problem 01 - 100%

---
+++ 07.Generics/07.Generics/P01.GenericBoxOfString/Box.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generics
+{
+    class Box<T>
+    {
+        public List<T> List { get; set; }
+
+        public Box()
+        {
+            List = new List<T>();
+        }
+
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder();
+
+            foreach (var item in List)
+            {
+                result.Append($"{item.GetType()}: {item}{Environment.NewLine}");
+            }
+
+            return result.ToString();
+                      
+        }
+    }
+}
---
+++ 07.Generics/07.Generics/P01.GenericBoxOfString/GenericBoxString.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Generics
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+
+            Box<string> box = new Box<string>();
+
+            for (int i = 0; i < count; i++)
+            {
+                string input = Console.ReadLine();
+                box.List.Add(input);
+            }
+
+            Console.WriteLine(box.ToString());
+        }
+    }
+}
