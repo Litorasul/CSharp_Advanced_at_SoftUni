c48c7074daa773e9659116beebb25f60dfa258af
Nikolay Kelov <n_kelov@yahoo.com>
2019-06-17 18:59:05 +0200

    Problem 5 - 100%

---
+++ 07.Generics/07.Generics/P05.GenericCountString/GenericCountString.cs
@@ -0,0 +1,24 @@
+﻿using Generics;
+using System;
+
+namespace P05.GenericCountString
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            Box<string> box = new Box<string>();
+
+            for (int i = 0; i < count; i++)
+            {
+                string input = Console.ReadLine();
+                box.List.Add(input);
+            }
+            string inp = Console.ReadLine();
+            box.GreaterThan(inp);
+
+            Console.WriteLine(box.GreaterElements.Count);
+        }
+    }
+}
