045471bb63f29b23e19fb5c464d08d3bbf0fbc09
Nikolay Kelov <n_kelov@yahoo.com>
2019-06-17 19:40:26 +0200

    Problem 07 - 100%

---
+++ 07.Generics/07.Generics/P07.Tuple/07.Tuple.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace P07.Tuple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine()
+                .Split();
+            MyTuple<string, string> tupleOne = new MyTuple<string, string>();
+            string itemOne = $"{input[0]} {input[1]}";
+            string itemTwo = input[2];
+            tupleOne.ItemOne = itemOne;
+            tupleOne.ItemTwo = itemTwo;
+
+            input = Console.ReadLine()
+                .Split();
+            MyTuple<string, int> tupleTwo = new MyTuple<string, int>();
+            string name = input[0];
+            int liters = int.Parse(input[1]);
+            tupleTwo.ItemOne = name;
+            tupleTwo.ItemTwo = liters;
+
+            input = Console.ReadLine()
+                .Split();
+            MyTuple<int, double> tupleThree = new MyTuple<int, double>();
+            int number = int.Parse(input[0]);
+            double floater = double.Parse(input[1]);
+            tupleThree.ItemOne = number;
+            tupleThree.ItemTwo = floater;
+
+            Console.WriteLine($"{tupleOne.ItemOne} -> {tupleOne.ItemTwo}");
+            Console.WriteLine($"{tupleTwo.ItemOne} -> {tupleTwo.ItemTwo}");
+            Console.WriteLine($"{tupleThree.ItemOne} -> {tupleThree.ItemTwo}");
+
+        }
+    }
+}
---
+++ 07.Generics/07.Generics/P07.Tuple/MyTuple.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P07.Tuple
+{
+    public class MyTuple<T, U>
+    {
+        public T ItemOne { get; set; }
+
+        public U ItemTwo { get; set; }
+
+    }
+}
