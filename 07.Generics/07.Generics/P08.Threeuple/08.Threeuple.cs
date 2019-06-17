7647db53e077786f795f48d2d2ae807e24a88b72
Nikolay Kelov <n_kelov@yahoo.com>
2019-06-17 19:55:47 +0200

    Problem 08 - 100%

---
+++ 07.Generics/07.Generics/P08.Threeuple/08.Threeuple.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace P08.Threeuple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine()
+                .Split();
+           var tupleOne = new MyThreeuple<string, string, string>();
+            string itemOne = $"{input[0]} {input[1]}";
+            string itemTwo = input[2];
+            string itemThree = input[3];
+            tupleOne.ItemOne = itemOne;
+            tupleOne.ItemTwo = itemTwo;
+            tupleOne.ItemThree = itemThree;
+
+            input = Console.ReadLine()
+                .Split();
+            var tupleTwo = new MyThreeuple<string, int, bool>();
+            string name = input[0];
+            int liters = int.Parse(input[1]);
+            bool drunkOrNot = false;
+            if (input[2] == "drunk")
+            {
+                drunkOrNot = true;
+            }
+            tupleTwo.ItemOne = name;
+            tupleTwo.ItemTwo = liters;
+            tupleTwo.ItemThree = drunkOrNot;
+
+            input = Console.ReadLine()
+                .Split();
+            var tupleThree = new MyThreeuple<string, double, string>();
+            string number = input[0];
+            double floater = double.Parse(input[1]);
+            string bankName = input[2];
+            tupleThree.ItemOne = number;
+            tupleThree.ItemTwo = floater;
+            tupleThree.ItemThree = bankName;
+
+            Console.WriteLine($"{tupleOne.ItemOne} -> {tupleOne.ItemTwo} -> {tupleOne.ItemThree}");
+            Console.WriteLine($"{tupleTwo.ItemOne} -> {tupleTwo.ItemTwo} -> {tupleTwo.ItemThree}");
+            Console.WriteLine($"{tupleThree.ItemOne} -> {tupleThree.ItemTwo} -> {tupleThree.ItemThree}");
+        }
+    }
+}
---
+++ 07.Generics/07.Generics/P08.Threeuple/MyThreeuple.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P08.Threeuple
+{
+    class MyThreeuple<T, U, V>
+    {
+        public T ItemOne { get; set; }
+        public U ItemTwo { get; set; }
+        public V ItemThree { get; set; }
+
+
+    }
+}
