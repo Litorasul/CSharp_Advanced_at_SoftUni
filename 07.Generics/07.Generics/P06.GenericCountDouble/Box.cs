c61fdd76516776c781160586391431801b3a5f38
Nikolay Kelov <n_kelov@yahoo.com>
2019-06-17 19:03:50 +0200

    Problem 06 - 100%

---
+++ 07.Generics/07.Generics/P06.GenericCountDouble/Box.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Generics
+{
+    public class Box<T> 
+        where T : IComparable<T>
+    {
+        public List<T> List { get; set; }
+
+        public List<T> GreaterElements { get; set; }
+
+        public Box()
+        {
+            List = new List<T>();
+            GreaterElements = new List<T>();
+        }
+
+        public void GreaterThan(T element)
+        {
+            foreach (var item in List)
+            {
+                if (item.CompareTo(element) == 1)
+                {
+                    GreaterElements.Add(item);
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder();
+
+            foreach (var item in List)
+            {
+                result.Append($"{item.GetType()}: {item}{Environment.NewLine}");
+            }
+
+            return result.ToString();
+                      
+        }
+    }
+}
---
+++ 07.Generics/07.Generics/P06.GenericCountDouble/GenericCountDouble.cs
@@ -0,0 +1,24 @@
+﻿using Generics;
+using System;
+
+namespace _06.GenericCountDouble
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            Box<double> box = new Box<double>();
+
+            for (int i = 0; i < count; i++)
+            {
+                double input = double.Parse(Console.ReadLine());
+                box.List.Add(input);
+            }
+            double inp = double.Parse(Console.ReadLine());
+            box.GreaterThan(inp);
+
+            Console.WriteLine(box.GreaterElements.Count);
+        }
+    }
+}
