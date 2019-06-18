af752f504c421360f1dcf4c4de3f22c38348ee2c
Nikolay Kelov <n_kelov@yahoo.com>
2019-06-18 19:45:01 +0200

    Workshop 01 - Double Linked List / int

---
+++ W01.Workshop-CustomStructures/W01.CustomStructures/W01.CustomStructures/DoublyLinkedList.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace W01.CustomStructures
+{
+    class DoublyLinkedList
+    {
+        private class ListNode
+        {
+            public int Value { get; private set; }
+
+            public ListNode NextNode { get; set; }
+            public ListNode PreviousNode { get; set; }
+
+            public ListNode(int value)
+            {
+                Value = value;
+            }
+        }
+
+        private ListNode head;
+        private ListNode tail;
+
+        public int Count { get; private set; }
+
+        public void AddFirst(int value)
+        {
+            ListNode newHead = new ListNode(value);
+
+            if (Count == 0)
+            {
+                tail = head = newHead;
+            }
+            else
+            {
+                newHead.NextNode = head;
+                head.NextNode = newHead;
+                head = newHead;
+            }
+
+            Count++;
+        }
+
+        public void AddLast(int value)
+        {
+            ListNode newTail = new ListNode(value);
+            if (Count == 0)
+            {
+                tail = head = newTail;
+            }
+            else
+            {
+                newTail.PreviousNode = tail;
+                tail.NextNode = newTail;
+                tail = newTail;
+            }
+
+            Count++;
+        }
+
+        public int RemoveFirst()
+        {
+            IfEmptyThrowExeption();
+
+            int firstElement = head.Value;
+            head = head.NextNode;
+
+            if (head == null)
+            {
+                tail = null;
+            }
+            else
+            {
+                head.PreviousNode = null;
+            }
+
+            Count--;
+
+            return firstElement;
+        }
+
+        public int RemoveLast()
+        {
+            IfEmptyThrowExeption();
+
+            int lastElement = tail.Value;
+            tail = tail.PreviousNode;
+
+            if (tail == null)
+            {
+                head = null;
+            }
+            else
+            {
+                tail.NextNode = null;
+            }
+
+            Count--;
+
+            return lastElement;
+        }
+
+        public bool Contains(int value)
+        {
+            ListNode currentNode = head;
+
+            while (currentNode != null)
+            {
+                if (currentNode.Value == value)
+                {
+                    return true;
+                }
+
+                currentNode = currentNode.NextNode;
+            }
+
+            return false;
+        }
+
+        public int[] ToArray()
+        {
+            int[] array = new int[Count];
+
+            var currentNode = head;
+            int count = 0;
+
+            while (currentNode != null)
+            {
+                array[count++] = currentNode.Value;
+
+                currentNode = currentNode.NextNode;
+                
+            }
+
+            return array;
+        }
+
+        public void Print(Action<int> action)
+        {
+            ListNode currentNode = head;
+
+            while (currentNode != null)
+            {
+                action(currentNode.Value);
+                currentNode = currentNode.NextNode;
+            }
+        }
+
+        private void IfEmptyThrowExeption()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException("DoublyLinkedList is empty!");
+            }
+        }
+    }
+}
---
+++ W01.Workshop-CustomStructures/W01.CustomStructures/W01.CustomStructures/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace W01.CustomStructures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var linkedList = new DoublyLinkedList();
+
+            linkedList.AddFirst(1);
+            linkedList.AddFirst(0);
+            linkedList.AddLast(2);
+            linkedList.AddLast(3);
+            //linkedList.Print(Console.WriteLine);
+            //Console.WriteLine(linkedList.Count == 4);
+
+            //linkedList.RemoveFirst();
+            //linkedList.Print(Console.WriteLine);
+            //Console.WriteLine(linkedList.Count == 3);
+
+            //linkedList.RemoveLast();
+            //linkedList.Print(Console.WriteLine);
+            //Console.WriteLine(linkedList.Count == 2);
+
+
+            Console.WriteLine(linkedList.Contains(2));
+            Console.WriteLine(linkedList.Contains(9));
+        }
+    }
+}
