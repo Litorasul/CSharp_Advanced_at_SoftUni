bd26d5210344f539f297c8c682e058fb46b3af35
Nikolay Kelov <n_kelov@yahoo.com>
2019-06-20 19:52:02 +0200

    Problem 01 - 100%

---
+++ 08.IteratorsAndComparators/08.IteratorsAndComparators/P01.ListyIterator/ListyIterator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01.ListyIterator
+{
+    class ListyIterator<T>
+    {
+        private List<T> items;
+        private int index;
+
+        public ListyIterator(List<T> items)
+        {
+            this.items = items;
+            this.index = 0;
+        }
+
+        public bool Move()
+        {
+            if (HasNext())
+            {
+                this.index++;
+                return true;
+            }
+            return false;
+        }
+
+        public bool HasNext()
+        {
+            if (this.index < this.items.Count - 1)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public void Print()
+        {
+            if (items.Count == 0)
+            {
+                throw new InvalidOperationException("Invalid Operation!");
+            }
+            Console.WriteLine(items[index]);
+        }
+
+    }
+}
---
+++ 08.IteratorsAndComparators/08.IteratorsAndComparators/P01.ListyIterator/ProblemOne.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P01.ListyIterator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = string.Empty;
+            ListyIterator<string> listy = null;
+
+            while ((command = Console.ReadLine()) != "END")
+            {
+                try
+                {
+                    if (command.Contains("Create"))
+                    {
+                        List<string> items = command
+                            .Split().Skip(1).ToList();
+
+                        listy = new ListyIterator<string>(items);
+                    }
+                    else if (command == "Move")
+                    {
+                        Console.WriteLine(listy.Move());
+                    }
+                    else if (command == "Print")
+                    {
+                        listy.Print();
+                    }
+                    else if (command == "HasNext")
+                    {
+                        Console.WriteLine(listy.HasNext());
+                    }
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
