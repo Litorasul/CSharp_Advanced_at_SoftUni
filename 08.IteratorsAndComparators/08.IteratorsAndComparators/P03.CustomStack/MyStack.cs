44904f25b01cd06683b90fec0ce584c30c0a5a11
Nikolay Kelov <n_kelov@yahoo.com>
2019-06-21 12:08:18 +0200

    Problem 03 - 100%

---
+++ 08.IteratorsAndComparators/08.IteratorsAndComparators/P03.CustomStack/MyStack.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P03.CustomStack
+{
+    public class MyStack <T> : IEnumerable<T>
+    {
+
+        private List<T> innerArr;
+
+        public int Count { get; private set; }
+
+        public MyStack()
+        {
+            innerArr = new List<T>();
+            Count = 0;
+        }
+
+        public void Push(T element)
+        {
+            innerArr.Add(element);
+            Count++;
+        }
+
+        public T Pop()
+        {
+            CheckCount();
+
+            Count--;
+            T tempElement = innerArr[Count];
+            innerArr.RemoveAt(Count);
+
+            return tempElement;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {           
+           
+            for (int i = Count - 1; i >= 0; i--)
+            {
+                yield return innerArr[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        private void CheckCount()
+        {
+            if (Count == 0)
+            {
+                throw new IndexOutOfRangeException("No elements");
+            }
+        }
+    }
+}
---
+++ 08.IteratorsAndComparators/08.IteratorsAndComparators/P03.CustomStack/ProblemThree.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+
+namespace P03.CustomStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = string.Empty;
+
+            var theStack = new MyStack<int>();
+
+            while ((command = Console.ReadLine()) != "END")
+            {
+                try
+                {
+                    if (command.Contains("Push"))
+                    {
+                        command = command.Remove(0, 5);
+                        int[] elements = command
+                            .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                           .Select(int.Parse).ToArray();
+
+                        foreach (var item in elements)
+                        {
+                            theStack.Push(item);
+                        }
+                    }
+                    else if (command == "Pop")
+                    {
+                        theStack.Pop();
+                    }
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            foreach (var item in theStack)
+            {
+                Console.WriteLine(item);
+            }
+
+            foreach (var item in theStack)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+
+}
