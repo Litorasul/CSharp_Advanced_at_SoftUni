7d667c3e9bd2fca50c93a204a1511307fa2a7ba4
Nikolay Kelov <n_kelov@yahoo.com>
2019-06-22 12:50:16 +0200

    Problem 06

---
+++ 08.IteratorsAndComparators/08.IteratorsAndComparators/P06.EqualityLogic/ProblemSix.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P06.EqualityLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+
+            HashSet<Person> hash = new HashSet<Person>();
+            SortedSet<Person> sorted = new SortedSet<Person>();
+
+            for (int i = 0; i < count; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+
+                string name = input[0];
+                int age = int.Parse(input[1]);
+
+                var person = new Person(name, age);
+                hash.Add(person);
+                sorted.Add(person);
+            }
+
+            Console.WriteLine(hash.Count);
+            Console.WriteLine(sorted.Count);
+        }
+    }
+}
