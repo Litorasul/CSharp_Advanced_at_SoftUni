4b4d376fe49a055f2cdfff0fcadeb6bf01c98254
Nikolay Kelov <n_kelov@yahoo.com>
2019-06-22 11:25:03 +0200

    Problem 04 - 100%

---
+++ 08.IteratorsAndComparators/08.IteratorsAndComparators/P04.Froggy/Lake.cs
@@ -0,0 +1,42 @@
+﻿
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P04.Froggy
+{
+    public class Lake : IEnumerable<int>
+    {
+        private int[] stones;
+
+        public Lake(int[] input)
+        {
+            stones = input;
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (int i = 0; i < stones.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    yield return stones[i];
+                }
+            }
+
+            for (int i = stones.Length - 1; i >= 0; i--)
+            {
+                if (i % 2 != 0)
+                {
+                    yield return stones[i];
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ 08.IteratorsAndComparators/08.IteratorsAndComparators/P04.Froggy/ProblemFour.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace P04.Froggy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Lake lake = new Lake(input);
+
+            Console.WriteLine(string.Join(", ", lake));
+        }    
+    }
+}
