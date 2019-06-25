731c81422dbebde6307d1c8ccecb586cae8f0809
Nikolay Kelov <n_kelov@yahoo.com>
2019-06-25 20:00:40 +0200

    Problem 01 - first try 37%

---
+++ ExamPrepp01-From-24.02.19/EP01-from-24.02.19/P01.ClubParty/ClubParty.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P01.ClubParty
+{
+    class ClubParty
+    {
+        static void Main(string[] args)
+        {
+            int hallCapacity = int.Parse(Console.ReadLine());
+
+            var hallsReservations = new Dictionary<string, List<int>>();
+
+            string[] input = Console.ReadLine()
+                .Split();
+
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                bool isNumber = int.TryParse(input[i], out int reservation);
+                if (isNumber)
+                {
+                    if (hallsReservations.Count == 0)
+                    {
+                        continue;
+                    }
+                    for (int j = 0; j < hallsReservations.Count; j++)
+                    {
+                        var item = hallsReservations.ElementAt(j);
+                        var hallKey = item.Key;
+                        var hallValue = item.Value;
+                        bool isOverflowing = hallValue.Sum() + reservation > hallCapacity;
+                        if (isOverflowing)
+                        {
+                            Console.WriteLine($"{hallKey} -> { string.Join(", ", hallValue)}");
+                            hallsReservations.Remove(hallKey);                          
+                        }
+                        else
+                        {
+                            hallValue.Add(reservation);                                                       
+                        }
+                    }
+                    
+                }
+                else
+                {
+                    hallsReservations[input[i]] = new List<int>(hallCapacity);
+                }
+            }
+
+        }
+    }
+}
